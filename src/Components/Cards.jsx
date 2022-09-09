267c905283622b5796f97610ad09d9ef438be7c6
Luis Rosales <luchorosalesc6@gmail.com>
2022-09-08 20:37:25 -0500

    Se arreglÂó la forma en que se mostraban las tarjetas

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -3,7 +3,7 @@ import Carousel from 'carousel-react-rcdev'
 
 export const Cards = ({ nombre, correo, celular, imagen }) => {
   return (
-    <Carousel>
+    
       <div className="m-10 bg-gradient-to-b from-cyan-500 to-blue-500 bg-white py-8 px-10 rounded-3xl shadow-xl w-96 mt-16 border-white border-4 text-center mb-16 ">
         <img
           className="w-36 -mt-16 shadow-xl rounded-full mb-4 border-white border-4 mx-auto"
@@ -13,7 +13,7 @@ export const Cards = ({ nombre, correo, celular, imagen }) => {
         <h2 className="text-white font-semibold">📩{correo}</h2>
         <h2 className="text-white font-bold">📲{celular}</h2>
       </div>
-      </Carousel>
+     
    
   );
 };
